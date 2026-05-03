2de69cb072485c3e4e989ab57a0b31e1bf62bb2f
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-05-03 16:29:50 +0300

    feat: add review service

---
+++ services/reviewService.ts
@@ -0,0 +1,31 @@
+import { prisma } from "../prisma/client";
+
+export const calcAverageRatings = async (productId: string) => {
+  const stats = await prisma.review.aggregate({
+    where: { productId },
+    _avg: {
+      rating: true,
+    },
+    _count: {
+      rating: true,
+    },
+  });
+
+  if (stats._count.rating > 0) {
+    await prisma.product.update({
+      where: { id: productId },
+      data: {
+        ratingsQuantity: stats._count.rating,
+        ratingsAverage: stats._avg.rating ?? 4.5,
+      },
+    });
+  } else {
+    await prisma.product.update({
+      where: { id: productId },
+      data: {
+        ratingsQuantity: 0,
+        ratingsAverage: 4.5,
+      },
+    });
+  }
+};
