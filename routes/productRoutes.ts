23773cd8d40c5461c1df498634cb7f1de7744326
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-05-05 17:00:22 +0300

    feat: add product routes

---
+++ routes/productRoutes.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import {
+  getAllProducts,
+  getSpecificProduct,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+  aliasTopProducts,
+  getProductStats,
+  uploadProductImages,
+  uploadProductImagesToFirebase,
+} from "../controllers/productController";
+import { protect, restrictTo } from "../controllers/authController";
+// import reviewRouter from "./reviewRoutes.js";
+
+const router = express.Router({ mergeParams: true });
+
+// router.use("/:productId/reviews", reviewRouter);
+
+router.route("/top-5-cheap").get(aliasTopProducts, getAllProducts);
+
+router.route("/product-stats").get(getProductStats);
+
+router
+  .route("/")
+  .get(getAllProducts)
+  .post(
+    protect,
+    restrictTo("admin"),
+    uploadProductImages,
+    uploadProductImagesToFirebase,
+    addProduct,
+  );
+
+router
+  .route("/:id")
+  .get(getSpecificProduct)
+  .patch(
+    protect,
+    restrictTo("admin"),
+    uploadProductImages,
+    uploadProductImagesToFirebase,
+    updateProduct,
+  )
+  .delete(protect, restrictTo("admin"), deleteProduct);
+
+export default router;
