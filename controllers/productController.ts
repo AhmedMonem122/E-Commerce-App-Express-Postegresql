1634d12a9bc72742d69b40a9a95449553da5a56e
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-05-13 14:17:58 +0300

    feat!: update product controller images uploader function

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -6,7 +6,14 @@ import AppError from "../utils/appError";
 import admin from "../config/firebase";
 
 // ✅ IMPORT FACTORY (PRISMA VERSION)
-import { getAll, getOne, addOne, updateOne, deleteOne } from "./handlerFactory";
+import {
+  getAll,
+  getOne,
+  addOne,
+  updateOne,
+  deleteOne,
+  uploadImageToFirebase,
+} from "./handlerFactory";
 
 // ==============================
 // FIREBASE
@@ -40,56 +47,9 @@ export const uploadProductImages = upload.fields([
 // ==============================
 // FIREBASE UPLOAD
 // ==============================
-export const uploadProductImagesToFirebase = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const files = req.files as {
-      imageCover?: Express.Multer.File[];
-      images?: Express.Multer.File[];
-    };
-
-    if (!files?.imageCover || !files?.images) return next();
-
-    // cover
-    const cover = files.imageCover[0];
-
-    const coverName = `Products/product-${req.params.id || ""}-${
-      cover.originalname
-    }-cover-${Date.now()}`;
-
-    const coverRef = storage.file(coverName);
-
-    await coverRef.save(cover.buffer, {
-      metadata: { contentType: cover.mimetype },
-    });
-
-    const coverURL = `https://firebasestorage.googleapis.com/v0/b/${
-      process.env.FIREBASE_STORAGE_BUCKET
-    }/o/${encodeURIComponent(coverName)}?alt=media`;
-
-    // images
-    const imagesURLs = await Promise.all(
-      files.images.map(async (file, i) => {
-        const name = `Products/product-${req.params.id || ""}-${
-          file.originalname
-        }-${Date.now()}-${i}`;
-
-        const ref = storage.file(name);
-
-        await ref.save(file.buffer, {
-          metadata: { contentType: file.mimetype },
-        });
-
-        return `https://firebasestorage.googleapis.com/v0/b/${
-          process.env.FIREBASE_STORAGE_BUCKET
-        }/o/${encodeURIComponent(name)}?alt=media`;
-      }),
-    );
-
-    req.body.imageCover = coverURL;
-    req.body.images = imagesURLs;
-
-    next();
-  },
+export const uploadProductImagesToFirebase = uploadImageToFirebase(
+  "Products",
+  storage,
 );
 
 // ==============================
