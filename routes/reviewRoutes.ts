61e052fb887d1e39006f6ae582a7c48f36060d5b
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-05-16 17:01:37 +0300

    feat: add review routes

---
+++ routes/reviewRoutes.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  getAllReviews,
+  getReview,
+  setProductUserIds,
+  addReview,
+  updateReview,
+  deleteReview,
+} from "../controllers/reviewController";
+import { protect, restrictTo } from "../controllers/authController";
+
+const router = express.Router({ mergeParams: true });
+
+router.use(protect);
+
+router
+  .route("/")
+  .get(getAllReviews)
+  .post(protect, restrictTo("user"), setProductUserIds, addReview);
+
+router
+  .route("/:id")
+  .get(getReview)
+  .patch(protect, restrictTo("user", "admin"), updateReview)
+  .delete(protect, restrictTo("user", "admin"), deleteReview);
+
+export default router;
