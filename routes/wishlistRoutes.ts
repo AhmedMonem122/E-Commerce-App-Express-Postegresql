46c9567d8f790012e44a0498bb7a1bcc3c59e17c
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-05-16 17:04:04 +0300

    feat: add wishlist routes

---
+++ routes/wishlistRoutes.ts
@@ -0,0 +1,17 @@
+import express = require("express");
+import {
+  getLoggedUserWishlist,
+  addProductToWishlist,
+  removeProductFromWishlist,
+} from "../controllers/wishlistController";
+import { protect, restrictTo } from "../controllers/authController";
+
+const router = express.Router();
+
+router.use(protect, restrictTo("user"));
+
+router.route("/").get(getLoggedUserWishlist).post(addProductToWishlist);
+
+router.delete("/:productId", removeProductFromWishlist);
+
+export default router;
