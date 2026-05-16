f6ac848b09aac58db415fae299560851c325b793
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-05-16 16:58:53 +0300

    feat: add payment routes

---
+++ routes/paymentRoutes.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import {
+  createCheckoutSession,
+  getUserPayments,
+  getAllPayments,
+  addPayment,
+  getSpecificPayment,
+  updatePayment,
+  deletePayment,
+} from "../controllers/paymentController";
+import { protect, restrictTo } from "../controllers/authController";
+
+const router = express.Router();
+
+router.use(protect);
+
+router.post("/checkout-session", createCheckoutSession);
+
+router.get("/myPayments", getUserPayments);
+
+router.use(restrictTo("admin"));
+
+router.route("/").get(getAllPayments).post(addPayment);
+
+router
+  .route("/:id")
+  .get(getSpecificPayment)
+  .patch(updatePayment)
+  .delete(deletePayment);
+
+export default router;
