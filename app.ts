0ba00c3e2d5a4ebeb66b33d564073af4ab37cff6
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-05-16 17:07:56 +0300

    feat!: update app && add all routes in it

--- app.ts
+++ app.ts
@@ -1,26 +1,26 @@
 import express, { type NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import productRouter from "./routes/productRoutes";
-// const categoryRouter = require("./routes/categoryRoutes");
-// const brandRouter = require("./routes/brandRoutes");
-// const wishlistRouter = require("./routes/wishlistRoutes");
-// const cartRouter = require("./routes/cartRoutes");
-// const reviewRouter = require("./routes/reviewRoutes");
+import categoryRouter from "./routes/categoryRoutes";
+import brandRouter from "./routes/brandRoutes";
+import wishlistRouter from "./routes/wishlistRoutes";
+import cartRouter from "./routes/cartRoutes";
+import reviewRouter from "./routes/reviewRoutes";
 import userRouter from "./routes/userRoutes";
-// const paymentRouter = require("./routes/paymentRoutes");
+import paymentRouter from "./routes/paymentRoutes";
 import AppError from "./utils/appError";
 import globalErrorHandler from "./controllers/errorController";
 import bodyParser from "body-parser";
 import cors from "cors";
-// const paymentController = require("./controllers/paymentController");
+import { webhookCheckout } from "./controllers/paymentController";
 
 const app = express();
 
-// app.post(
-//   "/webhook-checkout",
-//   express.raw({ type: "application/json" }),
-//   paymentController.webhookCheckout
-// );
+app.post(
+  "/webhook-checkout",
+  express.raw({ type: "application/json" }),
+  webhookCheckout,
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
@@ -31,13 +31,13 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use("/api/v1/products", productRouter);
-// app.use("/api/v1/categories", categoryRouter);
-// app.use("/api/v1/brands", brandRouter);
-// app.use("/api/v1/wishlist", wishlistRouter);
-// app.use("/api/v1/cart", cartRouter);
-// app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/categories", categoryRouter);
+app.use("/api/v1/brands", brandRouter);
+app.use("/api/v1/wishlist", wishlistRouter);
+app.use("/api/v1/cart", cartRouter);
+app.use("/api/v1/reviews", reviewRouter);
 app.use("/api/v1/users", userRouter);
-// app.use("/api/v1/payment", paymentRouter);
+app.use("/api/v1/payment", paymentRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
