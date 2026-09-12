8d81ed1a7cd0fcd075f7a4d88261e82575390fa2
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-09-12 17:18:19 +0300

    feat!: update server file to have swagger log

--- server.ts
+++ server.ts
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-
 dotenv.config({ path: "./.env" });
 
 import app from "./app.js";
@@ -8,4 +7,5 @@ const port = process.env.PORT || 6000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+  console.log(`[swagger]: Docs available at http://localhost:${port}/api-docs`);
 });
