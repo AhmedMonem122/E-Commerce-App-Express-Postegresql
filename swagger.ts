6146c85c8656cd2f0822beb357a96c67f7720c80
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-09-12 17:17:12 +0300

    feat: add swagger config file

---
+++ swagger.ts
@@ -0,0 +1,22 @@
+import swaggerAutogen from "swagger-autogen";
+import dotenv from "dotenv";
+
+dotenv.config();
+const port = process.env.PORT || 4000;
+
+const doc = {
+  info: {
+    title: "E-Commerce API",
+    description: "REST API for E-Commerce application",
+    version: "1.0.0",
+  },
+  host: `localhost:${port}`,
+  schemes: ["http"],
+  basePath: "",
+};
+
+const outputFile = "./swagger-output.json";
+
+const endpointsFiles = ["./app.ts"];
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
