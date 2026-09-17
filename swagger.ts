e772c27bc252498d1b9920e3e4e0c3232e2aa2b5
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-09-17 16:57:12 +0300

    feat: add query parameters components to swagger file

--- swagger.ts
+++ swagger.ts
@@ -2,6 +2,7 @@ import swaggerAutogen from "swagger-autogen";
 import dotenv from "dotenv";
 
 dotenv.config();
+
 const port = process.env.PORT || 4000;
 
 const doc = {
@@ -10,15 +11,69 @@ const doc = {
     description: "REST API for E-Commerce application",
     version: "1.0.0",
   },
+
   host: `localhost:${port}`,
   schemes: ["http"],
   basePath: "",
+
   components: {
     securitySchemes: {
       bearerAuth: {
         type: "http",
         scheme: "bearer",
         bearerFormat: "JWT",
+      },
+    },
+
+    parameters: {
+      pageParam: {
+        name: "page",
+        in: "query",
+        required: false,
+        schema: {
+          type: "integer",
+          default: 1,
+        },
+      },
+
+      limitParam: {
+        name: "limit",
+        in: "query",
+        required: false,
+        schema: {
+          type: "integer",
+          default: 40,
+        },
+      },
+
+      sortParam: {
+        name: "sort",
+        in: "query",
+        required: false,
+        schema: {
+          type: "string",
+          example: "-createdAt,price",
+        },
+      },
+
+      fieldsParam: {
+        name: "fields",
+        in: "query",
+        required: false,
+        schema: {
+          type: "string",
+          example: "title,price,description",
+        },
+      },
+
+      searchParam: {
+        name: "search",
+        in: "query",
+        required: false,
+        schema: {
+          type: "string",
+          example: "iphone",
+        },
       },
     },
   },
