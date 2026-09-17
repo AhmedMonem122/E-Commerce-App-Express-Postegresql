3a3600bb42c5b6c4fb0201e0bbe1ea93247412c8
AhmedMonem122 <ahmed.monem90@yahoo.com>
2026-09-17 16:58:48 +0300

    feat: add query parameters to get all && security authorization swagger to some category routes

--- routes/categoryRoutes.ts
+++ routes/categoryRoutes.ts
@@ -18,10 +18,34 @@ router.use("/:categoryId/products", filterByCategories, productRouter);
 
 router
   .route("/")
-  .get(getAllCategories)
+  .get(
+    /*
+      #swagger.parameters['page'] = {
+        $ref: '#/components/parameters/pageParam'
+      }
+      #swagger.parameters['limit'] = {
+        $ref: '#/components/parameters/limitParam'
+      }
+      #swagger.parameters['sort'] = {
+        $ref: '#/components/parameters/sortParam'
+      }
+      #swagger.parameters['fields'] = {
+        $ref: '#/components/parameters/fieldsParam'
+      }
+      #swagger.parameters['search'] = {
+        $ref: '#/components/parameters/searchParam'
+      }
+    */
+    getAllCategories,
+  )
   .post(
     protect,
     restrictTo("ADMIN"),
+    /*
+    #swagger.security = [{
+      bearerAuth: []
+    }]
+  */
     uploadCategoryImage,
     uploadCategoryImageToSupabase,
     addCategory,
@@ -33,10 +57,24 @@ router
   .patch(
     protect,
     restrictTo("ADMIN"),
+    /*
+    #swagger.security = [{
+      bearerAuth: []
+    }]
+  */
     uploadCategoryImage,
     uploadCategoryImageToSupabase,
     updateCategory,
   )
-  .delete(protect, restrictTo("ADMIN"), deleteCategory);
+  .delete(
+    protect,
+    restrictTo("ADMIN"),
+    /*
+    #swagger.security = [{
+      bearerAuth: []
+    }]
+  */
+    deleteCategory,
+  );
 
 export default router;
